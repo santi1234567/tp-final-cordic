c860b992f1830f1cd874ee6767ca1373604dd181
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2022-10-03 18:25:34 -0300

    Fixed issue with one less stage

--- sources/cordic.vhd
+++ sources/cordic.vhd
@@ -24,7 +24,7 @@ end;
 architecture cordic_arq of cordic is
 
 	component cordic_sub is
-		generic(N: integer:= N);
+		generic(N: integer:= N; ETAPA: integer);
 		port(
 			z_i		: in std_logic_vector(N-1 downto 0);
 		    y_i		: in std_logic_vector(N-1 downto 0);
@@ -37,7 +37,7 @@ architecture cordic_arq of cordic is
 			ena_i	: in std_logic
 		);
 	end component;
-	type T_AUX_PIPELINE is array (0 to ETAPAS) of std_logic_vector(N-1 downto 0);
+	type T_AUX_PIPELINE is array (0 to ETAPAS+1) of std_logic_vector(N-1 downto 0);
 
 	signal aux_z, aux_y, aux_x: T_AUX_PIPELINE := (others =>(others => '0'));
 	signal aux_z_scaled, aux_y_scaled, aux_x_scaled: std_logic_vector(2*N-1 downto 0) := (others => '0');
@@ -60,27 +60,32 @@ begin
 	aux_y(0) <= y_i;
 	aux_x(0) <= x_i;
 
-	cordicgen: for i in 0 to ETAPAS-1 generate
-			cordic_sub_inst: cordic_sub
-				port map(
-					clk_i 	=> clk_i,
-					rst_i 	=> rst_i,
-					ena_i 	=> ena_i,
-					x_i	 	=> aux_x(i),
-					y_i	 	=> aux_y(i),
-					z_i	 	=> aux_z(i),
-					x_o	 	=> aux_x(i+1),
-					y_o	 	=> aux_y(i+1),
-					z_o	 	=> aux_z(i+1)
-				);
+	cordicgen: for i in 0 to ETAPAS generate
+		cordic_sub_inst: cordic_sub
+			generic map(
+				N => N, 
+				ETAPA => i
+			)
+			port map(
+				clk_i 	=> clk_i,
+				rst_i 	=> rst_i,
+				ena_i 	=> ena_i,
+				x_i	 	=> aux_x(i),
+				y_i	 	=> aux_y(i),
+				z_i	 	=> aux_z(i),
+				x_o	 	=> aux_x(i+1),
+				y_o	 	=> aux_y(i+1),
+				z_o	 	=> aux_z(i+1)
+			);
 	end generate;
 	g <= to_signed(integer(GAIN(ETAPAS)*real((2**N)-1)/real(2)/math_pi),N); -- uso valores entre 0 y 2pi.
-	aux_z_scaled <= std_logic_vector(signed(aux_z(ETAPAS)) * g);
+	--aux_z_scaled <= std_logic_vector(signed(aux_z(ETAPAS)) * g);
 	--aux_y_scaled <= std_logic_vector(signed(aux_y(ETAPAS)) * g);
 	--aux_x_scaled <= std_logic_vector(signed(aux_x(ETAPAS)) * g);
 	--z_o <= aux_z_scaled(2*N-1 downto N);
 	--y_o <= aux_y_scaled(2*N-1 downto N);
 	--x_o <= aux_x_scaled(2*N-1 downto N);
-
-
+	z_o <= aux_z(ETAPAS+1);
+	y_o <= aux_y(ETAPAS+1);
+	x_o <= aux_x(ETAPAS+1);
 end;
