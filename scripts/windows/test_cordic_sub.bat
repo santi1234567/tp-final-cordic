d929bd453b50b51f4c3bedf5c1440941fd2cf401
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2022-10-03 18:24:46 -0300

    Changed simulation durations

--- scripts/windows/test_cordic_sub.bat
+++ scripts/windows/test_cordic_sub.bat
@@ -1,5 +1,5 @@
 ghdl -a sources\registro.vhd sources\cordic_sub.vhd simulation\tb_cordic_sub.vhd
 ghdl -s sources\registro.vhd sources\cordic_sub.vhd simulation\tb_cordic_sub.vhd
 ghdl -e cordic_sub_tb
-ghdl -r cordic_sub_tb --vcd=cordic_sub.vcd --stop-time=1000ns
+ghdl -r cordic_sub_tb --vcd=cordic_sub.vcd --stop-time=300ns
 gtkwave cordic_sub.vcd
