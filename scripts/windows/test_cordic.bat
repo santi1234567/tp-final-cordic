946ab53366776ae9a0224604357f6b7a0ac7cf75
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2022-10-03 12:56:33 -0300

    Updated batch scripts

--- scripts/windows/test_cordic.bat
+++ scripts/windows/test_cordic.bat
@@ -1,5 +1,5 @@
-ghdl -a cordic.vhd tb_cordic.vhd
-ghdl -s cordic.vhd tb_cordic.vhd
+ghdl -a sources\registro.vhd sources\cordic_sub.vhd sources\cordic.vhd simulation\tb_cordic.vhd
+ghdl -s sources\registro.vhd sources\cordic_sub.vhd sources\cordic.vhd simulation\tb_cordic.vhd
 ghdl -e cordic_tb
 ghdl -r cordic_tb --vcd=cordic.vcd --stop-time=1000ns --ieee-asserts=disable
 gtkwave cordic.vcd
--- scripts/windows/test_registro.bat
+++ scripts/windows/test_registro.bat
@@ -1,5 +1,5 @@
-ghdl -a registro.vhd tb_registro.vhd
-ghdl -s registro.vhd tb_registro.vhd
+ghdl -a sources\registro.vhd simulation\tb_registro.vhd
+ghdl -s sources\registro.vhd simulation\tb_registro.vhd
 ghdl -e registro_tb
 ghdl -r registro_tb --vcd=registro.vcd --stop-time=1000ns
 gtkwave registro.vcd
