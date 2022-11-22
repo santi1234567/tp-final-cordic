5fdd34ebf454e818b3bc55fda9e54529ce292b58
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2022-11-22 18:16:48 -0300

    fixed errors

--- sources/top.vhd
+++ sources/top.vhd
@@ -37,18 +37,20 @@ begin
             clk_pin     => 	clk_pin,
             rst_pin     => rst_pin,
             rxd_pin     => rxd_pin,
+            pixel_c_i   => pixel_c,
+            value_o     => value,
             gamma_i		=> gamma, -- rotation for z
             beta_i		=> beta, -- rotation for y
             alfa_i		=> alfa  -- rotation for x
         );
 
-    gamma <=    (15 downto 6 => rot_z,
-                others => '1');
-    beta <=    (15 downto 6 => rot_y,
-                others => '1');
-    alfa <=    (15 downto 6 => rot_x,
-                others => '1');
-                
+    gamma(15 downto 7) <= rot_z;
+    gamma(6 downto 0) <= (others => '0');
+    beta(15 downto 7) <= rot_y;
+    beta(6 downto 0) <= (others => '0');
+    alfa(15 downto 7) <= rot_x;
+    alfa(6 downto 0) <= (others => '0');
+
     identifier : process( clk_pin )
     begin
         if rising_edge(clk_pin) then
@@ -59,7 +61,7 @@ begin
                     rot_x_c <= (others => '0');
                     rot_x <= std_logic_vector(unsigned(rot_x) +1);                    
                 end if;
-            end if ;
+            end if;
             if btn_y = '1' then
                 if unsigned(rot_y_c) < 2500000 then
                     rot_y_c <= std_logic_vector(unsigned(rot_y_c) +1);
@@ -67,7 +69,7 @@ begin
                     rot_y_c <= (others => '0');
                     rot_y <= std_logic_vector(unsigned(rot_y) +1);                    
                 end if;
-            end if ;
+            end if;
             if btn_z = '1' then
                 if unsigned(rot_z_c) < 2500000 then
                     rot_z_c <= std_logic_vector(unsigned(rot_z_c) +1);
@@ -75,8 +77,8 @@ begin
                     rot_z_c <= (others => '0');
                     rot_z <= std_logic_vector(unsigned(rot_z) +1);                    
                 end if;
-            end if ;
+            end if;
         end if;
-    end process ; -- identifier
+    end process; -- identifier
 
 end top_arch;
